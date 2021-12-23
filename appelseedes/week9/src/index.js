d5d0d9432ba4ce310fff4de8804e152621d5d0ce
brurya-e <brurya.zak@gmail.com>
2021-12-23 14:05:26 +0200

    weekend1

---
+++ appelseedes/week9/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+// import App from './assignment/12.1/component/App'
+
+import App from './assignment/weekend1/App'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
